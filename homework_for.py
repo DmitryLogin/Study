ab8c737b658eb5d2aea46e8663e957427667d794
DmitryLogin <165498782+DmitryLogin@users.noreply.github.com>
2024-04-18 10:56:44 +0500

    Add files via upload

---
+++ homework_for.py
@@ -0,0 +1,9 @@
+machine_sheet = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+
+cars_count = 0
+for a in machine_sheet:
+    print(f'Я езжу на автомабиле марки {a}')
+    cars_count += 10
+    print(cars_count)
+
+
