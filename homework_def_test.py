e253f64782f39b7ae933ba704be78dd03fd9b72b
DmitryLogin <165498782+DmitryLogin@users.noreply.github.com>
2024-04-22 10:11:30 +0500

    Add files via upload

---
+++ homework_def_test.py
@@ -0,0 +1,13 @@
+def def_test(a, b):
+    print(a)
+    print(b)
+
+def_test('hi', 11)
+
+def def_test2(a, b, c):
+    print(a, b, c)
+    print(a)
+    print(b)
+    print(c)
+
+def_test2('hi', 11, True)
