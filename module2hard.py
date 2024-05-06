73ecc2fbb91be7082fc7d57d0b505537ca3b2faf
DmitryLogin <165498782+DmitryLogin@users.noreply.github.com>
2024-05-06 08:51:17 +0500

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -3,7 +3,7 @@ number_1 = int(input('Введите число от 3 до 20: '))
 
 
 list_number = []
-for i in range(number_1):
+for i in range(int(number_1 / 2)):
     number_2 = (i + 1)
     for a in range(number_1):
         number_3 = (a + 1)
