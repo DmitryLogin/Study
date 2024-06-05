e32ec83ed0744397e6afcc180bd63847f0a7bb4c
DmitryLogin <165498782+DmitryLogin@users.noreply.github.com>
2024-06-05 14:02:23 +0500

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,24 @@
+
+class Building():
+    def __init__(self, floor, buildingType):
+        self.numberOfFloors = floor
+        self.buildingType = buildingType
+
+    def __eq__(self, other):
+        return self.numberOfFloors == self.buildingType
+
+    def __lt__(self, other):
+        return self.numberOfFloors < other.numberOfFloors
+
+    def __gt__(self, other):
+        return self.numberOfFloors > other.numberOfFloors
+
+    def __ne__(self, other):
+        return self.numberOfFloors != other.numberOfFloors
+
+
+h1 = Building(23, 'ЖК Горский')
+h2 = Building(52, 'ЖК Горский')
+print(h1 == h2)
+print(h1 < h2)
+print(h1 != h2)
