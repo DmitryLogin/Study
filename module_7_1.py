fea4fb433c279b778c2937197fb4fdf3a7dcccaf
DmitryLogin <165498782+DmitryLogin@users.noreply.github.com>
2024-09-10 09:50:38 +0500

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -1,3 +1,4 @@
+
 class Product:
     def __init__(self, name, weight, category):
         self.name = str(name)
@@ -8,12 +9,15 @@ class Product:
         return f'{self.name}, {self.weight}, {self.category}'
 
 
-class Shop(Product):
+class Shop():
+
+    def __init__(self):
+        self.__file_name = 'products.txt'
     def get_products(self):
-        __file_name = 'products.txt'
-        file = open(__file_name, 'r')
-        return file.read()
+        file = open(self.__file_name, 'r')
+        products = file.read()
         file.close()
+        return products
 
     def add(self, *products):
         existing_products = self.get_products()
@@ -26,7 +30,7 @@ class Shop(Product):
                     file.write(f'{product}\n')
 
 
-s1 = Shop(1, 1, 1)
+s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
